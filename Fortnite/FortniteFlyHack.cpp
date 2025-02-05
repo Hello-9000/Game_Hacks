acdef4d07cea231759623207e692a87f57169d15
Liggy-Lang <liggylang@gmail.com>
2025-02-05 06:04:07 -0500

    FortniteFlyHack.cpp
    This script modifies the player's gravity to allow for flying. Note that this hack is highly dependent on the game's mechanics and may not work as intended in all situations. Also, using this hack may result in your account being banned from the game

--- Fortnite/FortniteFlyHack.cpp
+++ Fortnite/FortniteFlyHack.cpp
@@ -1,1 +1,45 @@
+#include <windows.h>
+#include <vector>
+#include <algorithm>
 
+// Define the base address of the game executable
+DWORD baseAddress = 0x00400000;
+
+// Define the offset to the player gravity
+DWORD playerGravityOffset = 0x01A7EC80;
+
+// Function to retrieve the player gravity
+float getPlayerGravity(HANDLE hProcess, DWORD baseAddress, DWORD playerGravityOffset) {
+    float playerGravity;
+    ReadProcessMemory(hProcess, (LPVOID)(baseAddress + playerGravityOffset), &playerGravity, sizeof(float), NULL);
+    return playerGravity;
+}
+
+// Function to set the player gravity
+void setPlayerGravity(HANDLE hProcess, DWORD baseAddress, DWORD playerGravityOffset, float newGravity) {
+    WriteProcessMemory(hProcess, (LPVOID)(baseAddress + playerGravityOffset), &newGravity, sizeof(float), NULL);
+}
+
+int main() {
+    // Get the handle to the game process
+    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, GetCurrentProcessId());
+
+    // Get the base address of the game executable
+    baseAddress = (DWORD)GetModuleHandleA(NULL);
+
+    // Set the player gravity to 0 (fly mode)
+    setPlayerGravity(hProcess, baseAddress, playerGravityOffset, 0.0f);
+
+    while (true) {
+        // Keep the player gravity at 0
+        setPlayerGravity(hProcess, baseAddress, playerGravityOffset, 0.0f);
+
+        // Sleep for 1ms
+        Sleep(1);
+    }
+
+    // Close the handle to the game process
+    CloseHandle(hProcess);
+
+    return 0;
+}
